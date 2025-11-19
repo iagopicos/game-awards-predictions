5e0fe70a56abf994a046ce3dbe50a4489777b731
Iago Fernandez Picos <iagofernandez94@gmail.com>
2025-11-19 21:34:01 +0100

    GAM-14 add login component, fighting with styles WIP

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,10 +1,5 @@
-// src/components/login/LoginPage.tsx
-
 import { Container } from '@mantine/core';
 import { LoginForm } from './LoginForm';
-// Importa el formulario que acabamos de limpiar
-// Asumo que tu AuthenticationForm está en el mismo directorio
-
 export function Login() {
     return (
         <Container size={420} my={40}>
